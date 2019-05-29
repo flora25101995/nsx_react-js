8c00a746c90c55b48366d9e7be431209b76626fa
flora25101995 <flora25101995@gmail.com>
2019-05-29 15:12:46 +0700

    create header/footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderComp from './component/header-component/header-component'
+import FooterComp from './component/footer-component/footer-component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+      <HeaderComp></HeaderComp>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      <FooterComp></FooterComp>
+      </div>
     </div>
   );
 }
